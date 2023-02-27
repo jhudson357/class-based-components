ee2800b6a5c7835830d433884125c54df7205ff9
James Hudson <jmh357@comcast.net>
2023-02-27 11:49:18 -0600

    transform UserFinder to class-based comp

--- src/components/UserFinder.jsx
+++ src/components/UserFinder.jsx
@@ -9,28 +9,64 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ]
 
-const UserFinder = () => {
-  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS) 
-  const [searchTerm, setSearchTerm] = useState('') 
+// class-based component
+class UserFinder extends Component {
+  constructor() {
+    super()
+    this.state = {
+      filteredUsers: DUMMY_USERS,
+      searchTerm: ''
+    }
+  }
 
-  useEffect(() => {
-    setFilteredUsers(
-      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
-    ) 
-  }, [searchTerm]) 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))
+      })
+    }
+  }
 
-  const searchChangeHandler = (event) => {
-    setSearchTerm(event.target.value) 
-  } 
+  searchChangeHandler(event) {
+    this.setState({searchTerm: event.target.value})
+  }
 
-  return (
-    <Fragment>
-      <div className={classes.finder}>
-        <input type='search' onChange={searchChangeHandler} />
-      </div>
-      <Users users={filteredUsers} />
-    </Fragment>
-  ) 
-} 
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type='search' onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    )
+  }
+}
+
+
+// functional component
+// const UserFinder = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS) 
+//   const [searchTerm, setSearchTerm] = useState('') 
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     ) 
+//   }, [searchTerm]) 
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value) 
+//   } 
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type='search' onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   ) 
+// } 
 
 export default UserFinder 
